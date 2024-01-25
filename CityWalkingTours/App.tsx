44d15de9ff6be76e568277bf4abeb62657b1eb78
NikaKlokava <veronika.kazakevich1997@gmail.com>
2024-01-25 20:18:45 +0200

    feat: update app file

--- CityWalkingTours/App.tsx
+++ CityWalkingTours/App.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import {StyleSheet} from 'react-native';
 import LinearGradient from 'react-native-linear-gradient';
-import {CityScreen} from './src/screens/CityScreen';
-import {CitySelectionScreen} from './src/screens/CitySelectionScreen';
-import {HomeScreen} from './src/screens/HomeScreen';
+// import {CityScreen} from './src/screens/CityScreen';
+// import {CitySelectionScreen} from './src/screens/CitySelectionScreen';
+// import {HomeScreen} from './src/screens/HomeScreen';
 import {colors} from './src/utils/colors';
 import {DetailsScreen} from './src/screens/DetailsScreen';
 
