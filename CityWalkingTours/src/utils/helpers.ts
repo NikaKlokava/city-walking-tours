e9b6700dc54e62398d1e3d565b1db5dd94920fc2
NikaKlokava <veronika.kazakevich1997@gmail.com>
2024-02-12 19:28:21 +0200

    docs: update changes

--- CityWalkingTours/src/utils/helpers.ts
+++
@@ -1,8 +0,0 @@
-import { NativeScrollEvent, NativeSyntheticEvent } from "react-native";
-
-export const getIndex = (e: NativeSyntheticEvent<NativeScrollEvent>) => {
-    const totalWidth = e.nativeEvent.layoutMeasurement.width;
-    const xPos = e.nativeEvent.contentOffset.x;
-    const index = Math.floor(xPos / totalWidth);
-    return index;
-  };
