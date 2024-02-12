584649f2c4fc5547fc34655984cab44a3a1faf4f
NikaKlokava <veronika.kazakevich1997@gmail.com>
2024-02-12 14:26:35 +0200

    feat: add layout animation

--- CityWalkingTours/src/screens/WishlistScreen.tsx
+++ CityWalkingTours/src/screens/WishlistScreen.tsx
@@ -1,11 +1,10 @@
 import React, {lazy} from 'react';
 import {AppWrapper} from '../components/AppWrapper';
 
-import {ScrollView, StyleSheet, View} from 'react-native';
+import {FlatList, StyleSheet, View} from 'react-native';
 import {CategoryItem} from '../components/CategoryItem';
 import {WISHLIST_DATA, colors} from '../utils';
 import {Text} from '../components/base/Text';
-import {Line} from '../components/Line';
 
 export const WishlistScreen = () => {
   return (
@@ -17,34 +16,26 @@ export const WishlistScreen = () => {
 
 const WishlistContent = () => {
   return (
-    <ScrollView>
-      <View style={styles.container}>
-        <Text type={'primary'} color={colors.active_bright} center>
-          Wishlist
-        </Text>
-        <View style={styles.itemsContainer}>
-          {WISHLIST_DATA.map((item, index) => {
-            return (
-              <View style={styles.itemsContainer} key={index}>
-                <CategoryItem category={item} city={'Vilno'} liked />
-                <Line white />
-              </View>
-            );
-          })}
-        </View>
-      </View>
-    </ScrollView>
+    <>
+      <Text type={'primary'} color={colors.active_bright} center>
+        Wishlist
+      </Text>
+      <FlatList
+        data={WISHLIST_DATA}
+        renderItem={({item, index}) => (
+          <View key={index} style={styles.itemsContainer}>
+            <CategoryItem category={item} liked />
+          </View>
+        )}
+      />
+    </>
   );
 };
 
 const LazyWishlistContent = lazy(async () => ({default: WishlistContent}));
 
 const styles = StyleSheet.create({
-  container: {
-    rowGap: 20,
-    alignItems: 'center',
-  },
   itemsContainer: {
-    rowGap: 20,
+    marginVertical: 20,
   },
 });
